a3ef2a6cfb270dafe9f9f3a7768334aa25afb180
insharahkalam <insharahkalam19@gmail.com>
2026-05-17 10:22:42 +0500

    toast done

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <>
-      <Toaster position="top-right" toastOptions={{
-        duration: 3000,
-      }} />
+      <Toaster position="top-right"
+        toastOptions={{
+          duration: 2000,
+        }}
+      />
       <Routing />
     </>
   )
